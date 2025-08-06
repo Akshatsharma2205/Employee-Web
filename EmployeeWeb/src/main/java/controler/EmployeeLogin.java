b76deb2a2f8bab9a7d3320a1ee525364d2dc2ff3
Akshat Sharma <akshat.sharma2205@gmail.com>
2025-08-07 00:33:07 +0530

    Add Employee Dashbord and Error html file and also complite EmployeeLogin Servlet

--- EmployeeWeb/src/main/java/controler/EmployeeLogin.java
+++ EmployeeWeb/src/main/java/controler/EmployeeLogin.java
@@ -5,7 +5,11 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Employee;
+import model.EmployeeDao;
+
 import java.io.IOException;
+import java.util.LinkedList;
 
 @WebServlet("/EmployeeLogin")
 public class EmployeeLogin extends HttpServlet {
@@ -24,7 +28,20 @@ public class EmployeeLogin extends HttpServlet {
 			throws ServletException, IOException {
 		int employeeId = Integer.parseInt(request.getParameter("employee-id"));
 		String password = request.getParameter("employee-password");
-		
+		EmployeeDao db = new EmployeeDao();
+		LinkedList<Employee> li = db.getAllEmployee();
+		int flag = 0;
+		for (Employee em : li) {
+			if(employeeId == em.getEmployeeId()) {
+				flag++;
+				if(password.equals(em.getEmployeePassword())) {
+					response.sendRedirect("employeeDasbord.html");
+				}
+			}
+		}
+		if(flag == 0) {
+			response.sendRedirect("error.html");
+		}
 	}
 
 }
